03895f29fb5c6971e9646e8edf690c3bc8071945
syntel <syntel@PHX440G3-2815X0.syntelorg.com>
2018-04-11 11:34:56 -0400

    nothing

---
+++ src/main/java/bootcamp/dao/InventoryDao.java
@@ -0,0 +1,13 @@
+package bootcamp.dao;
+
+import bootcamp.model.order.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+public class InventoryDao {
+
+    public void receiveOrderFromCustomer(Order order){
+
+
+    }
+}
